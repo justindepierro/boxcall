1cbd27771ae637d3e9d50f6f63bbbea5be6e5d04
justindepierro <jdepierro@burkecatholic.org>
2025-07-11 14:27:40 -0400

    test husky hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,10 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
-import pluginPrettier from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig({
-  files: ['**/*.{js,jsx}'],
-  plugins: {
-    react: pluginReact,
-    prettier: pluginPrettier,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:prettier/recommended', // 🚨 Enables eslint-plugin-prettier + disables conflicting rules
-  ],
-  languageOptions: {
-    globals: globals.browser,
-  },
-  rules: {
-    'prettier/prettier': 'warn', // 👀 You’ll see red/yellow squiggles when code isn’t pretty
-  },
-});
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,jsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,jsx}'], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
