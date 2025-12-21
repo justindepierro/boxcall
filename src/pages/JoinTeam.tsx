63f92fe87907cbe274b6a0a27bcaf10785217432
Justin DePierro <jdepierro@burkecatholic.org>
2025-12-21 13:25:31 -0500

    chore(cleanup): remove redundant page entry stubs

--- src/pages/JoinTeam.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * JoinTeam Component
- * Re-exports from modularized JoinTeam folder
- *
- * @see ./JoinTeam/JoinTeam.tsx for main implementation
- */
-
-export { default } from "./JoinTeam/JoinTeam";
--- src/pages/PracticePlansPage.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * PracticePlansPage Component
- * Re-exports from modularized PracticePlansPage folder
- *
- * @see ./PracticePlansPage/PracticePlansPage.tsx for main implementation
- */
-
-export { default } from "./PracticePlansPage/PracticePlansPage";
